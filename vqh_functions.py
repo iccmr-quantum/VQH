3fb79c9ede409941494b76856569765e4a1a4f62
schwaeti <tim.schwaegerl@desy.de>
2023-01-25 15:51:16 +0100

    multiple hamiltonians test

--- vqh_functions.py
+++ vqh_functions.py
@@ -1,10 +1,11 @@
-from qiskit import Aer
-from qiskit.algorithms.minimum_eigensolvers import VQE
+from qiskit.algorithms.minimum_eigensolvers import SamplingVQE
 from qiskit.algorithms.optimizers import COBYLA
-from qiskit.primitives import BackendEstimator, BackendSampler
+from qiskit.primitives import Sampler
 from qiskit.circuit.library import EfficientSU2
 from qiskit_optimization import QuadraticProgram
+from qiskit.algorithms.optimizers import COBYLA, NFT, SPSA
 import numpy as np
+import copy
 import copy
 
 
@@ -33,7 +34,20 @@ def qubo_to_operator(qubo):
     return operator, variables_index
 
 
-def run_vqe(ansatz, operator, optimizer, initial_point):
+def return_optimizer(optimizer_name, maxiter):
+    '''Convenience function to return optimizer object'''
+
+    if optimizer_name == 'SPSA':
+        optimizer = SPSA(maxiter=maxiter)
+    elif optimizer_name == 'COBYLA':
+        optimizer = COBYLA(maxiter=maxiter)
+    elif optimizer_name == 'NFT':
+        optimizer = NFT(maxiter=maxiter)
+
+    return optimizer
+
+
+def run_sampling_vqe(ansatz, operator, optimizer, initial_point):
     '''Run VQE with given ansatz, operator, optimizer and initial point, store intermediate results for every iteration'''
 
     eval_counts = []
@@ -46,28 +60,96 @@ def run_vqe(ansatz, operator, optimizer, initial_point):
         parameterss.append(parameters)
         values.append(value)
         metadatas.append(metadata)
-    backend = Aer.get_backend('qasm_simulator')
-    estimator = BackendEstimator(backend)
-    vqe = VQE(estimator, ansatz, optimizer, initial_point=initial_point,
-              callback=store_intermediate_result)
+    sampler = Sampler()
+    vqe = SamplingVQE(sampler, ansatz, optimizer, initial_point=initial_point,
+                      callback=store_intermediate_result)
     result = vqe.compute_minimum_eigenvalue(operator)
     intermediate_info = {'eval_counts': eval_counts, 'values': values,
                          'parameterss': parameterss, 'metadatas': metadatas}
     return result, intermediate_info
 
 
-def sample_from_intermediate_info(ansatz, params):
-
-    backend = Aer.get_backend('qasm_simulator')
-    sampler = BackendSampler(backend)
-    job = sampler.run(ansatz, parameter_values=params)
+def intermediate_parameters_to_quasi_dist(ansatz, parameters):
+    '''Convert intermediate results to quasi distributions. This is done by sampling from the ansatz with the parameters obtained from each iteration of VQE'''
+    sampler = Sampler()
+    job = sampler.run(ansatz, parameter_values=parameters)
     result = job.result()
-    quasi_dists = result.quasi_dists
-    return quasi_dists
+    quasi_dist = result.quasi_dists[0]
+    quasi_dist = quasi_dist.binary_probabilities()
+    return quasi_dist
 
 
-def harmonize():
+def quasi_dists_to_loudness(quasi_dists, variables_index):
+    '''Convert list of quasi distributions to loudness'''
 
+    loudnesses = {v: np.zeros(len(quasi_dists)) for v in variables_index}
+    variables_index_invert = {v: k for k, v in variables_index.items()}
+    for iteration, quasi_dist in enumerate(quasi_dists):
+        for key, value in quasi_dist.items():
+            for index, bit in enumerate(key[::-1]):
+                if bit == '1':
+                    note = variables_index_invert[index]
+                    loudnesses[note][iteration] += value
+    return loudnesses
+
+
+def loudnesses_to_list_of_dicts(loudnesses):
+    '''Convert loudnesses to list of dicts, where each dict contains the loudness of each note for a given iteration'''
+    loudness_list_of_dicts = []
+    for note, loudness_list in loudnesses.items():
+        for i, loudness in enumerate(loudness_list):
+            if len(loudness_list_of_dicts) <= i:
+                loudness_list_of_dicts.append({})
+            loudness_list_of_dicts[i][note] = loudness
+    return loudness_list_of_dicts
+
+
+def harmonize(qubos, iterations, **kwargs):
+    '''Run harmonizer algorithm for list of qubos and list of iterations. VQE is performed for the i-th qubo for i-th number of iterations.'''
+    # loop over qubos
+    for count, qubo in enumerate(qubos):
+        operator, variables_index = qubo_to_operator(qubo)
+        optimizer = return_optimizer(
+            kwargs['optimizer_name'], iterations[count])
+        ansatz = EfficientSU2(num_qubits=len(
+            variables_index), reps=kwargs['reps'], entanglement=kwargs['entanglement'])
+        if count == 0:
+            initial_point = np.zeros(ansatz.num_parameters)
+        # copy ansatz to avoid VQE changing it
+        ansatz_temp = copy.deepcopy(ansatz)
+        result, intermediate_info = run_sampling_vqe(
+            ansatz_temp, operator, optimizer, initial_point)
+        parameterss = intermediate_info['parameterss']
+        quasi_dists = []
+        # compute quasi distributions for each iteration of VQE
+        for parameters in parameterss:
+            ansatz_temp = copy.deepcopy(ansatz)
+            ansatz_temp.measure_all()
+            quasi_dist = intermediate_parameters_to_quasi_dist(
+                ansatz_temp, parameters)
+            quasi_dists.append(quasi_dist)
+        if count == 0:
+            loudnesses = quasi_dists_to_loudness(
+                quasi_dists, variables_index)
+        else:
+            loudnesses_temp = quasi_dists_to_loudness(
+                quasi_dists, variables_index)
+            for key, value in loudnesses_temp.items():
+                loudnesses[key] = np.append(loudnesses[key], value)
+        # set initital point for next qubo to be the optimal point of the previous qubo
+        initial_point = result.optimal_point
+    return loudnesses
+
+
+def test_harmonize():
+
+    # specify all possible notes. This is one octave. For more octaves, just add more notes.
+    notes = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b']
+    config = {
+        'reps': 0,
+        'entanglement': 'linear',
+        'optimizer_name': 'COBYLA'
+    }
     # example simple c major. Different Hamiltonians with superposition of chords as ground state are possible.
     # beneficial negative weights for desired notes
     c_major = {
@@ -76,32 +158,37 @@ def harmonize():
         ('g', 'g'): -1.,
     }
     # penalty for other notes
+    # make sure all notes are defined in the qubo
     notes = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b']
     for note in notes:
         if (note, note) not in c_major:
             c_major[(note, note)] = 1.
 
-    operator, variables_index = qubo_to_operator(c_major)
-    # different ansatze different sounds
-    ansatz = EfficientSU2(num_qubits=len(notes), reps=1)
-    # different optimizers different sound
-    optimizer = COBYLA(maxiter=64)
-    # start with silence
-    initial_point = np.zeros(ansatz.num_parameters)
-    ansatz_temp = copy.deepcopy(ansatz)
-    result, intermediate_info = run_vqe(
-        ansatz_temp, operator, optimizer, initial_point)
-    parameterss = intermediate_info['parameterss']
-    quasi_dists = []
-    for parameters in parameterss:
-        ansatz_temp = copy.deepcopy(ansatz)
-        ansatz_temp.measure_all()
-        quasi_dist = sample_from_intermediate_info(ansatz_temp, parameters)[0]
-        quasi_dist = quasi_dist.binary_probabilities()
-        quasi_dists.append(quasi_dist)
-    print(variables_index)
-    print(quasi_dists[-1])
-    return quasi_dists
+    f_major = {
+        ('f', 'f'): -1.,
+        ('a', 'a'): -1.,
+        ('c', 'c'): -1.,
+    }
+    for note in notes:
+        if (note, note) not in f_major:
+            f_major[(note, note)] = 1.
 
-if __name__ == '__main__':
-    pass 
+    g_major = {
+        ('g', 'g'): -1.,
+        ('b', 'b'): -1.,
+        ('d', 'd'): -1.,
+    }
+    for note in notes:
+        if (note, note) not in g_major:
+            g_major[(note, note)] = 1.
+
+    qubos = [c_major, g_major, f_major]
+    iterations = [3, 3, 3]
+
+    loudnesses = harmonize(qubos, iterations, **config)
+    loudness_list_of_dicts = loudnesses_to_list_of_dicts(loudnesses)
+
+    return loudness_list_of_dicts
+
+
+
