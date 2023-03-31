4f636c0013dbbded862234e38bb7749b6cbd0b61
Itaborala <28213905+Itaborala@users.noreply.github.com>
2023-03-31 12:52:21 +0100

    Mel Filter Bank sonfification updates

--- generate_mel_fb.py
+++ generate_mel_fb.py
@@ -3,11 +3,11 @@ import numpy as np
 import soundfile as sf
 def generate_mel_filters():
 
-    melfb = lr.filters.mel(sr=48000, n_fft=2048, n_mels=12, norm=np.inf)
+    melfb = lr.filters.mel(sr=96000, n_fft=2048, n_mels=12, norm=np.inf)
 
     for i, fil in enumerate(melfb):
         print(i, fil)
-        sf.write(f"/home/itaborala/melfb{i}.wav", fil[:-1], 48000)
+        sf.write(f"/home/itabora_/melfb{i}.wav", fil[:-1], 96000)
 
 
 if __name__ == "__main__":
