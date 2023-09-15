08f5a5127100dd09c9a74e3f5972e3b656ea1c34
Itaborala <28213905+Itaborala@users.noreply.github.com>
2023-09-15 12:20:26 +0100

    update sc_functions

--- sc_functions.py
+++ sc_functions.py
@@ -4,10 +4,11 @@ import time
 
 
 global NOTESDICT
-NOTESDICT = {"c":"amp1", "c#":"amp2", "d":"amp3", "d#":"amp4", "e":"amp5", "f":"amp6", "f#":"amp7", "g":"amp8", "g#":"amp9", "a":"amp10", "a#":"amp11", "b":"amp12"}
+NOTESDICT = {"c":"amp1", "c#":"amp2", "d":"amp3", "d#":"amp4", "e":"amp5", "f":"amp6", "f#":"amp7", "g":"amp8", "g#":"amp9", "a":"amp10", "a#":"amp11", "b":"amp12", "new":"amp13"}
 global FREQDICT
 FREQDICT = {"c":60, "c#":61, "d":62, "d#":63, "e":64, "f":65, "f#":66, "g":67, "g#":68, "a":69, "a#":70, "b":71}
-
+global EXAMPLE
+EXAMPLE = {"l1":"amp1", "l2":"amp2", "l3":"amp3", "l4":"amp4", "l5":"amp5", "l6":"amp6", "l7":"amp7", "l8":"amp8"}
 
 def note_loudness(qd): # Deprecated --------------------
     global server
@@ -35,7 +36,7 @@ def note_loudness_multiple(loudnessstream):
     global server, NOTESDICT
 
 
-    labels = ["amp1", "amp2", "amp3", "amp4", "amp5", "amp6", "amp7", "amp8", "amp9", "amp10", "amp11", "amp12"]
+    labels = ["amp1", "amp2", "amp3", "amp4", "amp5", "amp6", "amp7", "amp8", "amp9", "amp10", "amp11", "amp12", "amp13"]
     loudness = np.zeros(12)
     args = dict(zip(labels,loudness))
     synth = Synth(server, "vqe_model1_son1", args)
@@ -44,6 +45,21 @@ def note_loudness_multiple(loudnessstream):
         print(state)
         for k, amp in state.items():
             synth.set(NOTESDICT[k], amp)
+        time.sleep(0.03)
+# Mapping #1 - Simple additive synthesis
+def note_loudness_multiple_8_qubits(loudnessstream):
+    global server, NOTESDICT
+
+
+    labels = ["amp1", "amp2", "amp3", "amp4", "amp5", "amp6", "amp7", "amp8"]
+    loudness = np.zeros(12)
+    args = dict(zip(labels,loudness))
+    synth = Synth(server, "qubit_8", args)
+
+    for state in loudnessstream:
+        print(state)
+        for k, amp in state.items():
+            synth.set(EXAMPLE[k], amp)
         time.sleep(0.03)
 
 # Mapping #5 - Atonal additive sysnthesis chords used in "Rasgar, Saber" (2023)
@@ -124,9 +140,9 @@ def note_cluster_intensity(loudnessstream, expect_values):
         print(f" shifted value: {(expect_values[v] - min(expect_values))/100}")
         shifted_value = (expect_values[v] - min(expect_values))/100
         for i, (k, amp) in enumerate(sorted_state.items()):
-            #sy = Synth(server, "vqe_son2", {"note": FREQDICT[k], "amp":amp})
-            sy = Synth(server, "vqe_son2", {"note": FREQDICT[k]+expect_values[v]-3, "amp":amp})
-            time.sleep(0.035+shifted_value)
+            sy = Synth(server, "vqe_son2", {"note": FREQDICT[k], "amp":amp})
+            # sy = Synth(server, "vqe_son2", {"note": FREQDICT[k]+expect_values[v]-3, "amp":amp})
+            time.sleep(0.005+shifted_value)
         #time.sleep(0.2)
 
 # Mapping #6 - Granular Synthesis Spatialization 4 qubits
@@ -170,6 +186,8 @@ def sonify(loudnessstream, expect_values, son_type=1):
         note_loudness_multiple_rs(loudnessstream, expect_values)
     elif son_type == 6:
         granular_triggers(loudnessstream, expect_values)
+    elif son_type == 7:
+        note_loudness_multiple_8_qubits(loudnessstream)
 
 # Ctrl - . equivalent to kill sounds in SC
 def freeall():
